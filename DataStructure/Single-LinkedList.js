82c9130ef7bb6a8c2cad0c36d870ea3a7eb38fc1
Aditya-Dev-ops <aditya.developer.33@gmail.com>
2024-08-08 09:41:09 +0530

    adding reverse method to linked-list in DataStructure

--- DataStructure/Single-LinkedList.js
+++ DataStructure/Single-LinkedList.js
@@ -119,6 +119,23 @@ class LinkedList {
        }
       return this.error;
     }
+    reverse(){
+      if(!this.head.next){
+        return this.head;
+      }
+      this.tail = this.head;
+      let first = this.head;
+      let second = first.next;
+      while(second){
+        let temp = second.next;
+        second.next = first;
+        first = second;
+        second = temp;
+      }
+      this.head.next = null;
+      this.head = first; 
+      return this.printlist();
+    }
   }
   
   const list = new LinkedList(10);
